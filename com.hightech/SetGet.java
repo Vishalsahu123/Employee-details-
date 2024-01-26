7b6b1995f5c5e65c37a4830769b0d45e429b2618
Vishal Sahu <119094850+Vishalsahu123@users.noreply.github.com>
2024-01-26 14:40:40 +0530

    Create SetGet.java
    This is Seter Geter file

---
+++ com.hightech/SetGet.java
@@ -0,0 +1,174 @@
+package com.hightech;
+public class SetGet {
+	private int id,pin;
+	String fname,lname,dob,phone,email,address,state,country,gender,mthname,fthname,married,disable,doj,design,depart,uname,upass;
+    float Hra,Ta,Da,Pf,Esi,Gross,Net,salary;
+	public void setId(int id) {
+		this.id=id;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setFname(String Fname) {
+		this.fname=Fname;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setLname(String Lname) {
+		this.lname=Lname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setDob(String DOB) {
+		this.dob=DOB;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setPhone(String Phone) {
+		this.phone=Phone;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setEmail(String Email) {
+		this.email=Email;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setAddress(String Address) {
+		this.address=Address;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setState(String State) {
+		this.state=State;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setPin(int Pin) {
+		this.pin=Pin;
+	}
+	public int getPin() {
+		return pin;
+	}
+	public void setCountry(String Country) {
+		this.country=Country;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setGender(String Gender) {
+		this.gender=Gender;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setMthname(String Mthname) {
+		this.mthname=Mthname;
+	}
+	public String getMthname() {
+		return mthname;
+	}
+	public void setFthname(String Fthname) {
+		this.fthname=Fthname;
+	}
+	public String getFthname() {
+		return fthname;
+	}
+	public void setMarried(String Married) {
+		this.married=Married;
+	}
+	public String getMarried() {
+		return married;
+	}
+	public void setDisabilities(String Disabilities) {
+		this.disable=Disabilities;
+	}
+	public String getDisabilities() {
+		return disable;
+	}
+	public void setDoj(String DOJ) {
+		this.doj=DOJ;
+	}
+	public String getDoj() {
+		return doj;
+	}
+	public void setDesign(String Design) {
+		this.design=Design;
+	}
+	public String getDesign() {
+		return design;
+	}
+	public void setDepart(String Depart) {
+		this.depart=Depart;
+	}
+	public String getDepart() {
+		return depart;
+	}
+	public void setSalary(float Salary) {
+		this.salary=Salary;
+	}
+	public float getSalary() {
+		return salary;
+	}
+	public void setUname(String Uname) {
+		this.uname=Uname;
+	}
+	public String getUname() {
+		return uname;
+	}
+	public void setUpass(String Upass) {
+		this.upass=Upass;
+	}
+	public String getUpass() {
+		return upass;
+	}
+	public void setHra(float Hra) {
+		this.Hra=Hra;
+	}
+	public float getHra() {
+		return Hra;
+	}
+	public void setTa(float Ta) {
+		this.Ta=Ta;
+	}
+	public float getTa() {
+		return Ta;
+	}
+	public void setDa(float Da) {
+		this.Da=Da;
+	}
+	public float getDa() {
+		return Da;
+	}
+	public void setPf(float Pf) {
+		this.Pf=Pf;
+	}
+	public float getPf() {
+		return Pf;
+	}
+	public void setEsi(float Esi) {
+		this.Esi=Esi;
+	}
+	public float getEsi() {
+		return Esi;
+	}
+	public void setGross(float Gross) {
+		this.Gross=Gross;
+	}
+	public float getGross() {
+		return Gross;
+	}
+	public void setNet(float Net) {
+		this.Net=Net;
+	}
+	public float getNet() {
+		return Net;
+	}
+}
